ab6199f06bdc95db72a2d085517d73221640ae89
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-05-08 09:45:22 -0400

    virtual fields and methods

--- models/user.js
+++ models/user.js
@@ -31,8 +31,39 @@ const userSchema =new mongoose.Schema({
     },
     history:{
         type: Array,
-        default: 0
+        default: []
     }
 },
     {timestamps: true}
 );
+
+// Virtual fields
+userSchema.virtual('password')
+
+// setting password
+.set(function(password){
+    this._password = password
+    this.salt =uuidv1()
+    this.hashed_password = this.encryptPassword(password)
+})
+//getting password
+.get(function(){
+    return this._password
+})
+
+// creating encryptPassword method and adding to user schema
+userSchema.methods = {
+    encryptPassword: function(password){
+        if (!password) return "";
+        try{
+            return crypto
+                .createHmac("sha1", this.salt) // salt long complicated unique string
+                .update(password)
+                .digest("hex");
+        }catch(err){
+            return "";
+        }
+    }
+};
+
+module.exports = mongoose.model("User", userSchema);
