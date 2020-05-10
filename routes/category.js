46da6a4edcc304e97b5a0293aa133afbfc19d881
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-05-10 15:18:38 -0400

    created categories

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const{ create } = require("../controllers/category");
+const{userById} = require("../controllers/user");
+const{requireSignin, isAuth, isAdmin} = require("../controllers/auth");
 
 
 
-router.post("/category/create", create);
+router.post("/category/create/:userId", requireSignin, isAdmin, isAuth, create);
 
-
+router.param("userId", userById);
 module.exports = router;
