4939cfda2f14fd8ddfb6a1e9bcc655cca9d62de4
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-05-10 12:19:32 -0400

    authenticated and admin user complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,3 +63,24 @@ exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
     userProperty: "auth"
 });
+
+// authenticated user login method 
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if(!user){
+        return res.status(403).json({
+            error: "Access denied"
+        });
+    }
+    next();
+};
+
+// admin user login method
+exports.isAdmin = (req, res, next) => {
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error: "Admin users only! Access denied"
+        });
+    }
+    next();
+};
