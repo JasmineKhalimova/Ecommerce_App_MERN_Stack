c507e7082181118d0b35666a5a4f14a3b4d53a07
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-06-03 13:53:39 +0100

    payment system complete and connection to brain tree set up

--- routes/braintree.js
+++ routes/braintree.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const{requireSignin, isAuth} = require("../controllers/auth");
+const{userById} = require("../controllers/user");
+const{generateToken} = require("../controllers/braintree");
+
+router.get("/braintree/getToken/:userId", requireSignin, isAuth, generateToken); // Read - pulling braintree credentials info
+
+router.param("userId", userById);
+
+module.exports = router
