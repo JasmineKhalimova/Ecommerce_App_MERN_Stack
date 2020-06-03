1377e22ce4e6ffdb1ca3ecbdce64b3c61fe899ef
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-06-03 14:31:20 +0100

    payment processing method implemented

--- controllers/braintree.js
+++ controllers/braintree.js
@@ -9,12 +9,33 @@ const gateway = braintree.connect({
     privateKey: process.env.BRAINTREE_PRIVATE_KEY
 });
 
+// Brain Tree connecting method to generate token
 exports.generateToken = (req, res) => {
     gateway.clientToken.generate({}, function(err, response){
         if(err){
             res.status(500).send(err);
         }else{
             res.send(response);
+        }
+    });
+};
+
+// Process Payment Method
+exports.processPayment = (req, res) => {
+    let nonceFromTheClient = req.body.paymentMethodNonce
+    let amountFromClient = req.body.amount
+    //charge
+    let newTransaction = getway.transaction.sale({
+        amount: amountFromClient,
+        paymentMethodNonce: nonceFromTheClient,
+        options:{
+            submitForSettlement: true
+        }
+    }, (error, result) => {
+        if (error){
+            res.status(500).json(erro)
+        } else {
+            res.json(result)
         }
     });
 };
